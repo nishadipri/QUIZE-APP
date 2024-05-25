e1ccd3ee94c062389bfddb83c8a3be57c1b00f61
Nishadi <nish.shanika@gmail.com>
2024-05-25 15:37:07 +0200

    my first commit

---
+++ quize-Ts/src/App.tsx
@@ -0,0 +1,48 @@
+import React , {useState} from "react";
+import QuestionCard from "./components/QuestionCard";
+import { fetchQuestions } from "./API";
+import { Difficulty } from "./API";
+
+const App = () => {
+
+  const TOTAL_QUESTIONS = 10;
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+console.log(fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+  const startQuiz = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+
+
+  return <div className="App">
+    <h1>React Quiz</h1>
+
+    <button className="start" onClick={startQuiz}>Start Quiz</button>
+<p className="score">Score:</p>
+<p>Loading Questions ...</p>
+
+    <QuestionCard 
+     questionNr={number + 1}
+     totalQuestions={TOTAL_QUESTIONS}
+     question={questions[number].question}
+     answers={questions[number].answers}
+     userAnswer={userAnswers ? userAnswers[number] : undefined}
+     callback={checkAnswer}
+     />
+
+    <button className="next" onClick={nextQuestion}>Next Question</button>
+
+  </div>;
+};
+
+export default App;
